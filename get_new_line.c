4bcf4dc45b41c8755ecad1d57db71dc0cd6929fb
1stjanjona <jannat.hammouche@outlook.com>
2023-10-09 06:58:20 -0700

    new_line

---
+++ get_new_line.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+/**
+ * get_new_line - get new line
+ * Description: print new line
+ * Return: return (NULL)
+*/
+char *get_new_line(void)
+{
+	char *new_line = NULL;
+	size_t lngth = 0;
+	ssize_t nmbr;
+
+	if (is_ty(STDIN_FILENO))
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+	}
+	nmbr = get_new_line(&new_line, &lngth, stdin);
+	if (nmbr == -1)
+	{
+		free(new_line);
+		return (NULL);
+	}
+	return (NULL);
+}
