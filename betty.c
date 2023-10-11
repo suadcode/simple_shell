d20b5fc49373459b795ec58822d08097ca089b09
suadcode <suadsalam7@gmail.com>
2023-10-10 18:12:42 -0700

    update

---
+++ betty.c
@@ -0,0 +1,20 @@
+/*
+*  This function checks Betty passed
+* Args: None
+*
+* Returns:
+*   0  passed
+*   1  not passed
+*/
+function betty_checks(void)
+{
+
+betty_check_result = $(betty check)
+
+if [[$betty_check_result == "Passed"]];
+echo 0
+else
+echo 1
+}
+
+betty_checks
