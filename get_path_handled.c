d136638623947eec96fc25e8f822641b3885fb91
1stjanjona <jannat.hammouche@outlook.com>
2023-10-18 00:18:36 +0100

    path

--- get_path_handled.c
+++ get_path_handled.c
@@ -15,17 +15,6 @@ char *get_path_handled(char *pth)
 		{
 			if (passed(pth, &psd) == 0)
 				return (strdup(pth));
-			mk = malloc(sizeof(char) * (n + 1));
-			for (int i = 0; i < n; i++)
-				mk[i] = pth[i];
-			mk[n] = '\0', cmd_pth = malloc(sizeof(char) * (n + 1));
-			for (int i = 0; i < n; i++)
-				cmd_pth[i] = pth[i];
-			cmd_pth[n] = '\0', psd.path = mk, psd.cmd_path = cmd_pth, psd.next = NULL;
-			if (entire_path == NULL)
-				entire_path = malloc(sizeof(char) * (n + 1));
-			else
-				entire_path = realloc(entire_path, sizeof(char) * (n + 1));
 		}
 		else
 			return (NULL);
@@ -42,7 +31,8 @@ char *get_path_handled(char *pth)
 		_strcat(cmd_pth, mk), _strcat(cmd_pth, "/"), _strcat(cmd_pth, pth);
 
 	if (passed(cmd_pth, &psd) == 0)
-		free(entire_path), return (cmd_pth);
+		free(entire_path);
+	return (cmd_pth);
 	free(cmd_pth), cmd_pth = NULL, mk = strtok(NULL, ":");
 
 	free(entire_path);
