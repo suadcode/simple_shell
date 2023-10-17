1bffe5a2703166f91beecff7d382728e23985fe7
1stjanjona <jannat.hammouche@outlook.com>
2023-10-17 23:37:06 +0100

    new

--- string_func.c
+++ string_func.c
@@ -1,12 +1,10 @@
 #include "shell.h"
-
 /**
  * str_cat - copy string to another string.
  * @dest: destination string
  * @sorc: source string
  * Return: dest
  */
-
 char *str_cat(char *dest, char *sorc)
 {
 	int d = 0;
@@ -14,13 +12,13 @@ char *str_cat(char *dest, char *sorc)
 
 	while (dest[d] != '\0')
 	{
-	d++;
+		d++;
 	}
 	while (sorc[s] != '\0')
 	{
-	dest[d] = sorc[s];
-	d++;
-	s++;
+		dest[d] = sorc[s];
+		d++;
+		s++;
 	}
 	dest[d] = '\0';
 	return (dest);
@@ -32,17 +30,16 @@ char *str_cat(char *dest, char *sorc)
  * @s2: second compare  string
  * Return: 0
  */
-
 int str_comp(char *s1, char *s2)
 {
 	int a;
 
 	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
 	{
-	if (s1[a] != s2[a])
-	{
-	return ((int)s1[a] - s2[a]);
-	}
+		if (s1[a] != s2[a])
+		{
+			return ((int)s1[a] - s2[a]);
+		}
 	}
 	return (0);
 }
@@ -51,19 +48,16 @@ int str_comp(char *s1, char *s2)
  * @s: A pointer
  * Return: char a pointer to dest
  */
-
 int str_len(char *s)
 {
 	int chr = 0;
 
 	while (*(s + chr) != '\0')
 	{
-	chr++;
+		chr++;
 	}
-
 	return (chr);
 }
-
 /**
  * str_tocmp -  function that compares two strings.
  * @s1: first compare string
@@ -71,44 +65,40 @@ int str_len(char *s)
  * @num: number of characters
  * Return: result
  */
-
 size_t str_tocmp(char *s1, char *s2, size_t num)
 {
 	size_t i, j;
 
 	for (j = 0; s1[j] != '\0' && j < num; j++)
 	{
-	i = s1[j] - s2[j];
+		i = s1[j] - s2[j];
 
-	if (i != 0)
-	{
-	return (i);
-	}
+		if (i != 0)
+		{
+			return (i);
+		}
 	}
 	return (0);
 }
-
 /**
  * str_copy - copies the string pointed to by sorc into dest
  * @dest: destination of copy
  * @sorc: source of copy
  * Return: dest
  */
-
 char *str_copy(char *dest, char *sorc)
 {
 	int i = 0;
 
 	while (*(sorc + i) != '\0')
 	{
-	*(dest + i) = *(sorc + i);
-	++i;
+		*(dest + i) = *(sorc + i);
+		i++;
 	}
 	*(dest + i) = *(sorc + i);
 
 	return (dest);
 }
-
 /**
  * *_strncat - concatenates two strings
  *@dest: the first string
@@ -116,37 +106,24 @@ char *str_copy(char *dest, char *sorc)
  *@n: the amount of bytes to be maximally used
  *Return: the concatenated string
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
+	int i = 0, j = 0;
 	char *str = dest;
 
-
-	i = 0;
-	j = 0;
-
 	while (dest[i] != '\0')
-
-	i++;
-
-	while (src[j] != '\0' && j < n)
-
 	{
-
-	dest[i] = src[j];
-
-	i++;
-
-	j++;
-
+		i++;
 	}
-
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	if (j < n)
-
-	dest[i] = '\0';
-
+	{
+		dest[i] = '\0';
+	}
 	return (str);
-
 }
--- string_func_2.c
+++ string_func_2.c
@@ -1,240 +1,175 @@
 #include "shell.h"
-
-
-
 /**
  * *_strchr - locates a character in a string
  *@str: the string to be parsed
  *@chr: the character to look for
  *Return: (str) a pointer to the memory area s
  */
-
 char *_strchr(char *str, char chr)
-
 {
-
-	do {
-
-	if (*str == chr)
-
-	return (str);
-
-	}
-
-	while (*str++ != '\0');
-
+	do
+	{
+		if (*str == chr)
+		{
+			return (str);
+		}
+	} while (*str++ != '\0');
 	return (NULL);
-
 }
-
 /**
  * _strlen - returns the length of a string
  * @str: the string whose length to check
  * Return: integer length of string
  */
-
 int _strlen(char *str)
-
 {
-
 	int i = 0;
 
-
 	if (!str)
-
-	return (0);
-
-
+	{
+		return (0);
+	}
 	while (*str++)
-
-	i++;
-
+	{
+		i++;
+	}
 	return (i);
-
 }
-
-
 /**
  * _strcmp - performs lexicogarphic comparison of two strangs.
  * @str1: the first strang
  * @str2: the second strang
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-
 int _strcmp(char *str1, char *str2)
-
 {
-
 	while (*str1 && *str2)
-
 	{
-
-	if (*str1 != *str2)
-
-	return (*str1 - *str2);
-
-	str1++;
-
-	str2++;
-
+		if (*str1 != *str2)
+		{
+			return (*str1 - *str2);
+		}
+		str1++;
+		str2++;
 	}
-
 	if (*s1 == *s2)
-
-	return (0);
-
+	{
+		return (0);
+	}
 	else
-
-	return (*str1 < *str2 ? -1 : 1);
-
+	{
+		return (*str1 < *str2 ? -1 : 1);
+	}
 }
-
-
 /**
  * starts_with - checks if needle starts with haystack
  * @haystack: string to search
  * @needle: the substring to find
  * Return: address of next char of haystack or NULL
  */
-
 char *starts_with(const char *haystack, const char *needle)
-
 {
 
 	while (*needle)
-
-	if (*needle++ != *haystack++)
-
-	return (NULL);
-
+	{
+		if (*needle++ != *haystack++)
+		{
+			return (NULL);
+		}
+	}
 	return ((char *)haystack);
-
 }
-
-
 /**
  * _strcat - concatenates two strings
  * @dest: the destination buffer
  * @src: the source buffer
  * Return: pointer to destination buffer
  */
-
 char *_strcat(char *dest, char *src)
-
 {
 
 	char *del = dest;
 
-
 	while (*dest)
-
-	dest++;
-
+	{
+		dest++;
+	}
 	while (*src)
-
-	*dest++ = *src++;
-
-	*dest = *src;
-
+	{
+		*dest++ = *src++;
+		*dest = *src;
+	}
 	return (del);
-
-
 }
-
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
  * Return: pointer to the duplicated string
  */
-
 char *_strdup(const char *str)
 {
 
-	int length = 0;
-
+	int lnth = 0;
 	char *ret;
 
-
 	if (str == NULL)
-
-	return (NULL);
-
+	{
+		return (NULL);
+	}
 	while (*str++)
-
-	length++;
-
-	ret = malloc(sizeof(char) * (length + 1));
+	{
+		lnth++;
+	}
+	ret = malloc(sizeof(char) * (lnth + 1));
 
 	if (!ret)
-
-	return (NULL);
-
-	for (length++; length--;)
-
-	ret[length] = *--str;
-
+	{
+		return (NULL);
+	}
+	for (lnth++; lnth--;)
+	{
+		ret[lnth] = *--str;
+	}
 	return (ret);
-
 }
-
-
 /**
  * _puts - prints an input string
  * @str: the string to be printed
  * Return: Nothing
  */
-
 void _puts(char *str)
 {
 
 	int i = 0;
 
-
 	if (!str)
-
-	return;
-
+	{
+		return;
+	}
 	while (str[i] != '\0')
 	{
-
-	_putchar(str[i]);
-
-	i++;
-
+		_putchar(str[i]);
+		i++;
 	}
-
 }
-
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
-
 {
-	static int i;
-
+	static int i = 0;
 	static char buf[WRITE_BUF_SIZE];
 
-
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-
 	{
-
-	write(1, buf, i);
-
-	i = 0;
-
+		write(1, buf, i);
 	}
-
 	if (c != BUF_FLUSH)
-
-	buf[i++] = c;
-
+	{
+		buf[i++] = c;
+	}
 	return (1);
-
 }
