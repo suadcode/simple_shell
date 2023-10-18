9ed75db364828ab07f609d2d20def3bd00cf7c9e
1stjanjona <jannat.hammouche@outlook.com>
2023-10-18 09:56:15 -0700

    new correction

--- print_error.c
+++ print_error.c
@@ -8,44 +8,44 @@
  */
 void print_error(char *value, char *cmnd, int ndx)
 {
-    char *name;
-    char output[] = ": not found";
+	char *name;
+	char output[] = ": not found";
 
-    name = _itoa(ndx);
+	name = _itoa(ndx);
 
-    write(STDERR_FILENO, value, str_len(value));
-    write(STDERR_FILENO, ":", 2);
-    write(STDERR_FILENO, name, str_len(name));
-    write(STDERR_FILENO, ":", 2);
-    write(STDERR_FILENO, cmnd, str_len(cmnd));
-    write(STDERR_FILENO, output, str_len(output));
+	write(STDERR_FILENO, value, str_len(value));
+	write(STDERR_FILENO, ":", 2);
+	write(STDERR_FILENO, name, str_len(name));
+	write(STDERR_FILENO, ":", 2);
+	write(STDERR_FILENO, cmnd, str_len(cmnd));
+	write(STDERR_FILENO, output, str_len(output));
 
-    free(name);
+	free(name);
 }
 /**
  * _itoa - int to array
  * @n: integer
  * Return: buuffer
- */
+*/
 char *_itoa(int n)
 {
-    char buf[20];
-    int x = 0;
+	char buf[20];
+	int x = 0;
 
-    if (n == 0)
-    {
-        buf[x] = '0';
-        x++;
-    }
-    else
-    {
-        while (n > 0)
-        {
-            buf[x++] = (n % 10) + '0';
-            n /= 10;
-        }
-    }
-    buf[x] = '\0';
-    rev_str(buf, x);
-    return (_strdup(buf));
+	if (n == 0)
+	{
+		buf[x] = '0';
+		x++;
+	}
+	else
+	{
+		while (n > 0)
+		{
+			buf[x++] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	buf[x] = '\0';
+	rev_str(buf, x);
+	return (_strdup(buf));
 }
--- string_func1.c
+++ string_func1.c
@@ -1,21 +1,22 @@
+#include "shell.h"
 /**
  * rev_str - reverse string
  * @str: string
  * Return: no return
- */
+*/
 void rev_str(char *str, int lnth)
 {
-    int x;
-    char pass;
+	int x;
+	char pass;
 
-    while (str[lnth] != '\0')
-    {
-        lnth++;
-    }
-    while (x = 0, x < lnth / 2)
-    {
-        pass = str[x];
-        str[x] = str[lnth - x - 1];
-        str[lnth - x - 1] = pass;
-    }
+	while (str[lnth] != '\0')
+	{
+		lnth++;
+	}
+	while (x = 0, x < lnth / 2)
+	{
+		pass = str[x];
+		str[x] = str[lnth - x - 1];
+		str[lnth - x - 1] = pass;
+	}
 }
